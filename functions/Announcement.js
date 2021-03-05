7b92f7ed1b5648b7a84469620cbbff9151f3ff1d
Poxios <poxios0310@gmail.com>
2021-03-05 16:07:21 +0900

    delete announcement feature complete

--- functions/Announcement.js
+++ functions/Announcement.js
@@ -59,4 +59,64 @@ asyncRouter.post("/create", async (req, res, next) => {
   }
 });
 
+asyncRouter.post("/delete", async (req, res, next) => {
+  // body 추출
+  const { body } = req;
+
+  // 토큰 확인
+  const user = await tokenExporter(req.headers);
+  if (
+    user === ERRORS.AUTH.TOKEN_FAIL ||
+    user === ERRORS.AUTH.NO_AUTH_IN_HEADER
+  ) {
+    return next(user);
+  }
+
+  // body 유효성 체크.
+  if (body.docId === undefined) {
+    return next(ERRORS.DATA.INVALID_DATA);
+  }
+
+  console.log(
+    "이메일",
+    user.email,
+    "체크",
+    findStrInArray(user.email, adminEmails)
+  );
+
+  // 관리자인지 체크.
+  if (!findStrInArray(user.email, adminEmails)) {
+    return next(ERRORS.AUTH.NO_PERMISSION);
+  }
+
+  try {
+    await DB.announcement.doc(body.docId).delete();
+    res.status(200).send({ result: "Delete announcement success" });
+  } catch (err) {
+    console.log(err);
+    return next(ERRORS.DATA.INVALID_DATA);
+  }
+});
+
+asyncRouter.use((err, _req, res, _next) => {
+  switch (err) {
+    case ERRORS.DATA.INVALID_DATA:
+      res.status(400).send({ error: "Invalid data" });
+      break;
+    case ERRORS.AUTH.NO_AUTH_IN_HEADER:
+      res.status(401).send({ error: "No Auth in header" });
+      break;
+    case ERRORS.AUTH.TOKEN_FAIL:
+      res.status(401).send({ error: "Problem with token" });
+      break;
+    case ERRORS.AUTH.NO_PERMISSION:
+      res.status(401).send({ error: "No Permission" });
+      break;
+    default:
+      console.log(err);
+      res.status(500).send({ error: "INTERNAL ERROR" });
+      break;
+  }
+});
+
 module.exports = asyncRouter;
