6ef96083ba142328eb75cc31f62665c8a5f9b853
Poxios <poxios0310@gmail.com>
2021-09-21 21:37:43 +0900

    yes

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-exports.secrets = {
-  type: process.env.TYPE,
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.AUTH_URI,
-  token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-};
+// exports.secrets = {
+//   type: process.env.TYPE,
+//   project_id: process.env.PROJECT_ID,
+//   private_key_id: process.env.PRIVATE_KEY_ID,
+//   private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+//   client_email: process.env.CLIENT_EMAIL,
+//   client_id: process.env.CLIENT_ID,
+//   auth_uri: process.env.AUTH_URI,
+//   token_uri: process.env.TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+//   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+// };
+exports.secrets = JSON.parse(process.env.SECRETS);
 
 exports.mailAccount = {
   user: process.env.MAIL_USER,
